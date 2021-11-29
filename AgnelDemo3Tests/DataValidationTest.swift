54c79bf6b0ede2a5f96c8d4e4da88ad0ba69b604
AgnelSelvan <agnelselvan007@gmail.com>
2021-11-29 10:37:20 +0530

    Added Yaml File

---
+++ AgnelDemo3Tests/DataValidationTest.swift
@@ -0,0 +1,13 @@
+//
+//  DataValidationTest.swift
+//  AgnelDemo3Tests
+//
+//  Created by Neosoft on 25/11/21.
+//
+
+@testable import AgnelDemo3
+import XCTest
+
+class DataValidationTest: XCTestCase {
+    
+}
